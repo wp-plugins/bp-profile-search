fd4f88141fe7429e496bed340f42ea54d9cc99a3
dontdream <dontdream@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-14 23:08:40 +0000

    tagging version 2.0
    git-svn-id: https://plugins.svn.wordpress.org/bp-profile-search/tags/2.0@311449 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ps-admin.php
+++ ps-admin.php
@@ -18,6 +18,7 @@ function ps_set_default_options ()
 <p>You can find site members searching their public profiles. Search by any or all of the fields below:</p>';
 
 	$ps_options['fields'] = array ();
+	$ps_options['agerange'] = 0;
 
 	update_option ($ps_define->option_name, $ps_options);
 	return true;
@@ -47,8 +48,8 @@ function ps_admin_page ()
 	<?php $ps_options = get_option ($ps_define->option_name); ?>
 
 	<p>Customize your Profile Search here. Select the search message text 
-	and the profile fields to be included in the profile search form.</p>	
-	
+	and the profile fields to be included in the profile search form.</p>
+
 	<table class="form-table">
 
 	<tr valign="top"><th scope="row">Search Message:</th>
@@ -61,19 +62,21 @@ function ps_admin_page ()
 <?php if (bp_is_active ('xprofile')) : 
 	if (function_exists ('bp_has_profile')) : 
 		if (bp_has_profile ()) :
-			while (bp_profile_groups ()) : 
-				bp_the_profile_group(); 
+			$dateboxes = array ();
+			$dateboxes[0] = '';
 
-				echo '<strong>'. bp_get_the_profile_group_name(). ':</strong><br />';
+			while (bp_profile_groups ()) : 
+				bp_the_profile_group (); 
+
+				echo '<strong>'. bp_get_the_profile_group_name (). ':</strong><br />';
 
 				while (bp_profile_fields ()) : 
 					bp_the_profile_field(); 
 					switch (bp_get_the_profile_field_type ())
 					{
 					case 'datebox':	
-					case 'multiselectbox':	
-					case 'checkbox':
 						$disabled = 'disabled="disabled"';
+						$dateboxes[bp_get_the_profile_field_id ()] = bp_get_the_profile_field_name ();
 						break;
 					default:
 						$disabled = '';
@@ -97,6 +100,11 @@ endif; ?>
 
 	  </td>
 	</tr>
+
+<?php if (defined ('ENABLE_AGE_RANGE') && count ($dateboxes) > 1)
+	ps_select ('Age Range Field:', 'ps-options[agerange]', $dateboxes, $ps_options['agerange']);
+?>
+
 	</table>
 
 	<p class="submit">
@@ -107,6 +115,24 @@ endif; ?>
 </div>
 
 <?php
+}
+
+function ps_select ($label, $name, $list, $selected, $attr='', $comment='')
+{
+	echo "<tr valign=\"top\"><th scope=\"row\">$label</th>\n";
+	echo "<td><select name=\"$name\" $attr>\n";
+	foreach ($list as $value => $caption)
+		{
+			echo "<option value=\"$value\"";
+			if ($value == $selected) echo " selected=\"selected\"";
+			echo "> $caption &nbsp; </option>\n";
+		}
+	echo "</select>\n";
+		
+	echo "$comment\n";
+	echo "</td></tr>\n";
+
+	return true;
 }
 
 function ps_get_vars ($vars)
