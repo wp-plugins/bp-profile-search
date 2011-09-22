e80dd7e4d29654eaa2a805d95eb92305e3ad8079
dontdream <dontdream@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-22 13:41:49 +0000

    tagging version 2.6
    git-svn-id: https://plugins.svn.wordpress.org/bp-profile-search/tags/2.6@442108 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bps-main.php
+++ bps-main.php
@@ -3,7 +3,7 @@
 Plugin Name: BP Profile Search
 Plugin URI: http://www.blogsweek.com/bp-profile-search/
 Description: Search BuddyPress extended profiles.
-Version: 2.5
+Version: 2.6
 Author: Andrea Tarantini
 Author URI: http://www.blogsweek.com/
 */
